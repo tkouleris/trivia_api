3cd03a38f6f436f4c23e6fbb57e6ba5396c87bcd
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-01-29 06:32:59 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 import requests
+from urllib.parse import unquote
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    url = "https://opentdb.com/api.php?amount=10"
+    print(unquote("Alejandro%20G.%20I%C3%B1%C3%A1rritu"))
+    url = "https://opentdb.com/api.php?amount=10&encode=url3986"
     response = requests.get(url)
     return response.json()
