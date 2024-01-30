d4cf1d95f19136102f3c3a0e908ef77b9b747458
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-01-30 06:43:55 +0200

    refactor

---
+++ OpenTDB.py
@@ -0,0 +1,21 @@
+from QuestionTransformer import QuestionTransformer
+import requests
+
+
+class OpenTDB():
+    base_url = "https://opentdb.com/api.php?amount={amount}&category={category}&difficulty={difficulty}&encode=url3986"
+
+    def __init__(self):
+        self.transformer = QuestionTransformer()
+
+    def create_url(self, amount, category, difficulty):
+        final_url = self.base_url.replace("{amount}", amount)
+        final_url = final_url.replace("{category}", category)
+        final_url = final_url.replace("{difficulty}", difficulty)
+        return final_url
+
+    def get(self, amount, category, difficulty):
+        url = self.create_url(amount, category, difficulty)
+        response = requests.get(url)
+        json = response.json()
+        return self.transformer.run(json['results'])
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from flask import Flask
-import requests
-from urllib.parse import unquote
+# import requests
+# from urllib.parse import unquote
+from OpenTDB import OpenTDB
 
-from QuestionTransformer import QuestionTransformer
+# from QuestionTransformer import QuestionTransformer
 
 app = Flask(__name__)
 
 
-def myfunction():
-    return 0.5
-
-
 @app.route("/")
 def hello_world():
-    url = "https://opentdb.com/api.php?amount=10&encode=url3986"
-    response = requests.get(url)
-    json = response.json()
-    transformer = QuestionTransformer()
-    return transformer.run(json['results'])
+    opentdb = OpenTDB()
+    return opentdb.get("10", "9", 'easy')
+    # url = "https://opentdb.com/api.php?amount=10&encode=url3986"
+    # response = requests.get(url)
+    # json = response.json()
+    # transformer = QuestionTransformer()
+    # return transformer.run(json['results'])
