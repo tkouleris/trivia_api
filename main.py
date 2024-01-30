71be0165aa50a7926e867d4b9be31a073def1360
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-01-30 06:53:24 +0200

    refactor: categories constants

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask
-# import requests
-# from urllib.parse import unquote
-from OpenTDB import OpenTDB
 
-# from QuestionTransformer import QuestionTransformer
+from OpenTDB import OpenTDB
+import Categories
 
 app = Flask(__name__)
 
@@ -11,9 +9,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     opentdb = OpenTDB()
-    return opentdb.get("10", "9", 'easy')
-    # url = "https://opentdb.com/api.php?amount=10&encode=url3986"
-    # response = requests.get(url)
-    # json = response.json()
-    # transformer = QuestionTransformer()
-    # return transformer.run(json['results'])
+    return opentdb.get("10", Categories.GENERAL_KNOWLEDGE, 'easy')
