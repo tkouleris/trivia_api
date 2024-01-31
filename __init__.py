b6a9e411fcae9ec3c94502d3a7d592f04c0bc5c5
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-01-31 06:44:00 +0200

    fix: env

--- __init__.py
+++ __init__.py
@@ -1,15 +1,20 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
 
-    # app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
     db.init_app(app)
 
