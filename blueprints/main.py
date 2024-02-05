eb130df96416eed346b4ed47c0d9cd6e77e48de7
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-02-05 06:54:47 +0200

    fix: categories list

--- blueprints/main.py
+++ blueprints/main.py
@@ -1,20 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
 import Categories
 from OpenTDB import OpenTDB
 from app import token_required
 
 main = Blueprint('main', __name__)
 
-
-# app = Flask(__name__)
-
-@main.route('/test', methods=['GET'])
-@token_required
-def locked():
-    return "Locked"
-
-@main.route("/trivia/general")
+@main.route("/trivia", methods=['GET'])
 @token_required
 def hello_world():
     opentdb = OpenTDB()
-    return opentdb.get("10", Categories.GENERAL_KNOWLEDGE, 'easy')
+    return opentdb.get("10", Categories.list[request.args['cat'].upper()], 'easy')
